60e9f27c27b53638c12db3f6af9451acdc9a71ca
berrueta <berrueta@localhost>
2010-12-10 18:25:53 +0100

    Improved exception handling

--- parrot-core/src/main/java/es/ctic/parrot/reader/jena/JenaOWLReader.java
+++ parrot-core/src/main/java/es/ctic/parrot/reader/jena/JenaOWLReader.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Iterator;
 
 import org.apache.log4j.Logger;
+import org.xml.sax.SAXParseException;
 
 import com.hp.hpl.jena.ontology.Individual;
 import com.hp.hpl.jena.ontology.OntClass;
@@ -12,12 +13,14 @@ import com.hp.hpl.jena.ontology.OntProperty;
 import com.hp.hpl.jena.ontology.OntResource;
 import com.hp.hpl.jena.ontology.Ontology;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.shared.JenaException;
 import com.hp.hpl.jena.vocabulary.RDF;
 import com.hp.hpl.jena.vocabulary.RDFS;
 
 import es.ctic.parrot.de.DocumentableObjectRegister;
 import es.ctic.parrot.reader.DocumentReader;
 import es.ctic.parrot.reader.Input;
+import es.ctic.parrot.reader.ReaderException;
 
 public class JenaOWLReader implements DocumentReader {
     
@@ -26,15 +29,24 @@ public class JenaOWLReader implements DocumentReader {
 	/* (non-Javadoc)
 	 * @see es.ctic.parrot.reader.DocumentReader#readDocumentableObjects(es.ctic.parrot.reader.Input, es.ctic.parrot.de.DocumentableObjectRegister)
 	 */
-	public void readDocumentableObjects(Input input, DocumentableObjectRegister register) throws IOException {
+	public void readDocumentableObjects(Input input, DocumentableObjectRegister register) throws IOException, ReaderException {
         OntModel model = ModelFactory.createOntologyModel();
         String base = null;
-        logger.debug("Parsing OWL file");
-        model.read(input.openReader(), base, getJenaFormat(input));
-        loadOntology(model, register);
-        loadOntClasses(model, register);
-		loadOntProperties(model, register);
-		loadOntIndividuals(model, register);
+        
+        try {
+            logger.debug("Parsing OWL file");
+            model.read(input.openReader(), base, getJenaFormat(input));
+            loadOntology(model, register);
+            loadOntClasses(model, register);
+            loadOntProperties(model, register);
+            loadOntIndividuals(model, register);
+        } catch (JenaException e) {
+            if (e.getCause() != null && e.getCause() instanceof SAXParseException) {
+                throw new ReaderException("OWL parse error: " + e.getCause().getMessage(), e);
+            } else {
+                throw new ReaderException("While reading OWL file", e);
+            }
+        }
 	}
 
 	private static String getJenaFormat(Input input) {

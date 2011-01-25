b68376a1e2c16c6adfd36a3657a3cbd8dec7f96c
Diego <56665+berrueta@users.noreply.github.com>
2011-01-25 13:06:08 +0100

    Added kindString

--- parrot-core/src/main/java/es/ctic/parrot/de/DocumentableObject.java
+++ parrot-core/src/main/java/es/ctic/parrot/de/DocumentableObject.java
@@ -7,6 +7,23 @@ import es.ctic.parrot.transformers.DocumentableObjectVisitor;
 import es.ctic.parrot.transformers.TransformerException;
 
 public interface DocumentableObject {
+    
+    public enum Kind {
+        UNDEFINED("undefined"), 
+        ONTOLOGY("ontology"),
+        ONTOLOGY_CLASS("class"),
+        ONTOLOGY_PROPERTY("property"),
+        ONTOLOGY_INDIVIDUAL("individual"),
+        RULE("rule"),
+        RULE_SET("ruleset");
+        private final String name;
+        private Kind(String name) { 
+            this.name = name;
+        }
+        public String toString() {
+            return name;
+        }
+    };
 	
 	public abstract Object accept(DocumentableObjectVisitor visitor) throws TransformerException;
 	public abstract Identifier getIdentifier();
@@ -20,5 +37,6 @@ public interface DocumentableObject {
 	public abstract Collection<RelatedDocument> getRelatedDocuments(Locale locale);
 	public abstract Collection<Label> getLabels(Locale locale);
 	public abstract Collection<Label> getLabels();
+	public abstract String getKindString();
 
 }

69f1cc9c70e9fda0209a335bacb948bc3a5a9271
dayures <dayures@localhost>
2011-01-27 15:03:36 +0100

    removed not used imports

--- parrot-core/src/main/java/es/ctic/parrot/reader/rifle/RuleImpl.java
+++ parrot-core/src/main/java/es/ctic/parrot/reader/rifle/RuleImpl.java
@@ -32,7 +32,6 @@ import es.ctic.parrot.de.Label;
 import es.ctic.parrot.de.RelatedDocument;
 import es.ctic.parrot.de.Rule;
 import es.ctic.parrot.de.URIIdentifier;
-import es.ctic.parrot.de.DocumentableObject.Kind;
 import es.ctic.parrot.transformers.DocumentableObjectVisitor;
 import es.ctic.parrot.transformers.TransformerException;
 import es.ctic.parrot.utils.URIUtils;

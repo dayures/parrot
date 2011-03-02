293a9638e7c0e36715e4b11c19cc8d552f09af46
Carlos Tejo <carlos*dot*NO*SPAM*tejo*N*O**M*O*RE**SPAM*fundacionctic*dot*org@localhost>
2011-03-02 15:17:52 +0100

    added annotations

--- parrot-web/src/main/java/es/ctic/parrot/utils/Taglib.java
+++ parrot-web/src/main/java/es/ctic/parrot/utils/Taglib.java
@@ -3,14 +3,16 @@ package es.ctic.parrot.utils;
 import java.util.Map;
 
 public class Taglib {
-   public static boolean contains(Map map, Object o) {
+   @SuppressWarnings("rawtypes")
+public static boolean contains(Map map, Object o) {
 	   if (map == null)
 		   return false;
 	   else
 		   return map.containsKey(o);
    }
    
-   public static String getMessage(Map map, Object o) {
+   @SuppressWarnings("rawtypes")
+public static String getMessage(Map map, Object o) {
 	      return (String) map.get(o);
    }
 }

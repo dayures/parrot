bd1228a4373b5347c9f2143469ca0cf3aa803d47
dayures <dayures@users.noreply.github.com>
2011-03-04 10:03:48 +0100

    added javadoc for de package

---
+++ parrot-core/src/main/java/es/ctic/parrot/de/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package manages the heterogeneity of elements to document obtained from documents.
+ * 
+ * @author <a href="http://www.fundacionctic.org">CTIC Foundation</a>
+ * @version 1.0
+ * @since 1.0
+ */
+
+package es.ctic.parrot.de;

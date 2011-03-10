3fbdcea4e4644862594aceec898bf8037f2e282b
dayures <dayures@localhost>
2011-03-10 15:25:48 +0100

    Doc: improve javadoc

--- parrot-core/src/main/java/es/ctic/parrot/transformers/package-info.java
+++ parrot-core/src/main/java/es/ctic/parrot/transformers/package-info.java
@@ -1,5 +1,5 @@
 /**
- * This package holds the transformers that resolve cross references, creates a glossary... 
+ * This package holds transformers that resolve cross references, creates a glossary... 
  * @author <a href="http://www.fundacionctic.org">CTIC Foundation</a>
  * @version 1.0
  * @since 1.0

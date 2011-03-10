fd6543fa07a5a6a7c8934ee5806c41c7e4907178
dayures <dayures@localhost>
2011-03-10 16:57:39 +0100

    Doc: improve javadoc

--- parrot-core/src/main/java/es/ctic/parrot/docmodel/DocumentableObjectReference.java
+++ parrot-core/src/main/java/es/ctic/parrot/docmodel/DocumentableObjectReference.java
@@ -6,18 +6,38 @@ import java.util.Locale;
 
 import es.ctic.parrot.de.DocumentableObject;
 
+/**
+ * 
+ * A reference to a documentable element.
+ * 
+ * @author <a href="http://www.fundacionctic.org">CTIC Foundation</a>
+ * @version 1.0
+ * @since 1.0
+ *
+ */
 public class DocumentableObjectReference {
     
     private final String label;
     private String localName;
     private String kindString;
     
-    public DocumentableObjectReference(DocumentableObject documentableObject, Locale locale) {
+    /**
+     * Constructs a reference to a documentable element. 
+     * @param documentableObject the documentable element. 
+     * @param locale the locale.
+     */
+    private DocumentableObjectReference(DocumentableObject documentableObject, Locale locale) {
         this.label = documentableObject.getLabel(locale);
         this.localName = documentableObject.getLocalName();
         this.kindString = documentableObject.getKindString();
     }
     
+    /**
+     * Returns a collection of references to a documentable elements.
+     * @param documentableObjects the collection of documentable elements to reference.
+     * @param locale the locale.
+     * @return a collection of references to a documentable elements.
+     */
     public static Collection<DocumentableObjectReference> createReferences(Collection<? extends DocumentableObject> documentableObjects, Locale locale) {
         Collection<DocumentableObjectReference> result = new ArrayList<DocumentableObjectReference>(documentableObjects.size());
         for (DocumentableObject documentableObject : documentableObjects) {
@@ -26,23 +46,40 @@ public class DocumentableObjectReference {
         return result;
     }
     
+    /**
+     * Returns a reference to a documentable element.
+     * @param documentableObject the documentable element to reference.
+     * @param locale the locale.
+     * @return a reference to a documentable element.
+     */
     public static DocumentableObjectReference createReference(DocumentableObject documentableObject, Locale locale) {
     	if (documentableObject != null){
     		return new DocumentableObjectReference(documentableObject, locale);
     	} else {
     		return null;
     	}
-    		
     }
 
+    /**
+     * Returns the label for the referenced documentable element..
+     * @return the label for the referenced documentable element.
+     */
     public String getLabel() {
         return this.label;
     }
     
+	/**
+	 * Returns the unique anchor for the referenced documentable element.
+	 * @return the unique anchor for the referenced documentable element.
+	 */
     public String getLocalName() {
         return this.localName;
     }
     
+	/**
+	 * Returns the kind string for the referenced documentable element. 
+	 * @return the kind string for the referenced documentable element.
+	 */
     public String getKindString() {
         return this.kindString;
     }
--- parrot-core/src/main/java/es/ctic/parrot/docmodel/Glossary.java
+++ parrot-core/src/main/java/es/ctic/parrot/docmodel/Glossary.java
@@ -12,14 +12,32 @@ import com.ibm.icu.text.Collator;
 
 import es.ctic.parrot.de.DocumentableObject;
 
+/**
+ * 
+ * This class represents a glossary (an alphabetical list of terms with the references for those terms)
+ * 
+ * @author <a href="http://www.fundacionctic.org">CTIC Foundation</a>
+ * @version 1.0
+ * @since 1.0
+ * 
+ */
 public class Glossary {
 
     private final SortedMap<String, Set<DocumentableObject>> glossaryEntries;
     
+    /**
+     * Constructs a glossary for the given locale.
+     * @param locale the locale.
+     */
     public Glossary(Locale locale) {
         glossaryEntries = new TreeMap<String, Set<DocumentableObject>>(new GlossaryEntryComparator(locale));
     }
     
+    /**
+     * Adds an entry in the glossary.
+     * @param term the term.
+     * @param documentableObject the documentable element to be referenced.
+     */
     public void addReference(String term, DocumentableObject documentableObject) {
         Set<DocumentableObject> references = glossaryEntries.get(term);
         if (references == null) {
@@ -29,14 +47,26 @@ public class Glossary {
         references.add(documentableObject);
     }
     
+    /**
+     * Returns the glossary entries, sorted alphabetically.
+     * @return the glossary entries, sorted alphabetically.
+     */
     public SortedMap<String, Set<DocumentableObject>> getGlossaryEntries() {
         return Collections.unmodifiableSortedMap(glossaryEntries);
     }
     
+    /**
+     * Returns <code>true</code> if, and only if, number of entries of this glossary is 0.
+     * @return <code>true</code> if number of entries of this glossary is 0, otherwise <code>false</code>.
+     */
     public boolean isEmpty() {
         return glossaryEntries.isEmpty();
     }
     
+    /**
+     * Returns the number of entries of this glossary.
+     * @return the number of entries of this glossary.
+     */
     public int size() {
         return glossaryEntries.size();
     }
@@ -46,7 +76,7 @@ public class Glossary {
 class GlossaryEntryComparator implements Comparator<String> {
 
     private final Collator collator;
-    
+ 
     public GlossaryEntryComparator(Locale locale) {
         collator = Collator.getInstance(locale);
     }

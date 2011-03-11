f61a9993c4ecca28b349b98f8b05e2b8057b9f66
dayures <dayures@localhost>
2011-03-11 13:20:55 +0100

    Doc: improve javadoc

--- parrot-core/src/main/java/es/ctic/parrot/de/AnonymousIdentifier.java
+++ parrot-core/src/main/java/es/ctic/parrot/de/AnonymousIdentifier.java
@@ -1,5 +1,13 @@
 package es.ctic.parrot.de;
 
+/**
+ * An identifier for an element that is anonymous (it has not URI).
+ * 
+ * @author <a href="http://www.fundacionctic.org">CTIC Foundation</a>
+ * @version 1.0
+ * @since 1.0
+ * 
+ */
 public class AnonymousIdentifier implements Identifier {
 
     private final int internalId;

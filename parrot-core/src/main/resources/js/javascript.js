247aba92f54087c47cf5eb576d29eee515a9a2e6
dayures <dayures@localhost>
2011-03-23 17:06:13 +0100

    Change: added javascript flavour

--- parrot-core/src/main/resources/js/javascript.js
+++ parrot-core/src/main/resources/js/javascript.js
@@ -5,4 +5,15 @@ jQuery(document).ready(function(){
     $('.it').corner();
     $('.vci').corner();
     $('.otherinfo').corner();
+    
+    $('.term h3').click(function() {
+	    $(this).nextAll("div").toggle('slow');
+	    return false;
+    }).nextAll("div").hide();
+
+    $('.term div h4').click(function() {
+	    $(this).next().toggle('slow');
+	    return false;
+    }).next().hide();
+    
 })

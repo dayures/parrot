22e81a00bf0fc91183c3343c41d5796567d483ec
dayures <dayures@localhost>
2011-03-24 15:39:02 +0100

    Feature: added support for tooltips (qTip 1.0.0-rc3)

--- parrot-core/src/main/resources/js/javascript.js
+++ parrot-core/src/main/resources/js/javascript.js
@@ -35,5 +35,28 @@ jQuery(document).ready(function(){
         $(".term div div dl:visible").prevAll("h4").click();
       });
     
+    $.fn.qtip.styles.parrot = { 
+            border: {
+               width:1,
+               radius: 5,
+            },
+            padding: 10, 
+            textAlign: 'center',
+//            width: 300,
+            tip: true, // Give it a speech bubble tip with automatic corner detection
+            name: 'green' // Style it according to the preset 'cream' style
+      }
+
+      $.fn.qtip.defaults.position.corner = {
+           tooltip: 'bottomMiddle',
+           target: 'topMiddle'
+      }
+
+      $.fn.qtip.defaults.hide.when.event = 'mouseout';
+      $.fn.qtip.defaults.hide.fixed = true;
     
+      $('.icon-info-description').qtip({
+          content: 'Add a description using http://purl.org/dc/terms/description', // Use the tooltip attribute of the element for the content //
+          style: 'parrot'
+      });
 })

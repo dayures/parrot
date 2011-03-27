2b24cccfbef44f1c740791c1957ff9edb60977c9
Diego <56665+berrueta@users.noreply.github.com>
2011-03-27 22:14:40 +0200

    Added Dataset class

---
+++ parrot-core/src/main/java/es/ctic/parrot/reader/jena/DatasetJenaImpl.java
@@ -0,0 +1,26 @@
+package es.ctic.parrot.reader.jena;
+
+import com.hp.hpl.jena.ontology.OntResource;
+import com.hp.hpl.jena.rdf.model.Resource;
+
+import es.ctic.parrot.de.Dataset;
+import es.ctic.parrot.de.DocumentableObjectRegister;
+import es.ctic.parrot.transformers.DocumentableObjectVisitor;
+import es.ctic.parrot.transformers.TransformerException;
+
+public class DatasetJenaImpl extends AbstractJenaDocumentableObject implements Dataset {
+
+    public DatasetJenaImpl(OntResource resource, DocumentableObjectRegister register, OntResourceAnnotationStrategy annotationStrategy) {
+        super(resource, register, annotationStrategy);
+    }
+    
+    public Object accept(DocumentableObjectVisitor visitor)
+            throws TransformerException {
+        return visitor.visit(this);
+    }
+
+    public String getKindString() {
+        return Kind.DATASET.toString();
+    }
+
+}

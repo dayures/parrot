c7978fedd19b5a2921f6aa14b53322ee608333f1
dayures <dayures@localhost>
2011-03-28 12:54:08 +0200

    Feature: change default view for business and technical

--- parrot-core/src/main/resources/js/business.js
+++ parrot-core/src/main/resources/js/business.js
@@ -1,4 +1,8 @@
 jQuery(document).ready(function(){
     $('div.details.ontologies div.term h3').nextAll('div').show();
     $('div.details.ontologies div.term div h4').next('dl').show();
+    
+    $('.vci dl').show();
+
 })
+
---
+++ parrot-core/src/main/resources/js/technical.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function(){
+      $('.it dl').show();
+})

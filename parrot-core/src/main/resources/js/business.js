3b694a8714b8deb60c4ba3a8febb50f42530cee4
dayures <dayures@localhost>
2011-04-12 11:35:26 +0200

    Code: improve UX (velocity templates)

--- parrot-core/src/main/resources/js/business.js
+++ parrot-core/src/main/resources/js/business.js
@@ -3,8 +3,11 @@ jQuery(document).ready(function(){
     $('div.details.ontologies div.term h3 span.menu span').addClass('active');
 	$('div.details.ontologies div.term div div').show();
 	
-    $('.vci').show();
-    $('.menu span.vci-menu').addClass('active');
+    $('.inuse').show();
+    $('.menu span.inuse-menu').addClass('active');
+    
+    $('.otherinfo').show();
+    $('.menu span.otherinfo-menu').addClass('active');
 
 })
 

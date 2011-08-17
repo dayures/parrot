abf98cd0e0d239c0a99ede2408529f334ac987a3
dayures <dayures@localhost>
2011-08-17 14:29:17 +0200

    allow document multiple files using popup menu

--- parrot-eclipse/src/es/ctic/parrot/parrot_eclipse/utils/ParrotCoreWrapper.java
+++ parrot-eclipse/src/es/ctic/parrot/parrot_eclipse/utils/ParrotCoreWrapper.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.Locale;
+import java.util.Map;
 
 import es.ctic.parrot.DocumentaryProject;
 import es.ctic.parrot.DocumentaryProjectFactory;
@@ -66,6 +67,39 @@ public class ParrotCoreWrapper {
 		HtmlOutputGenerator generator = new HtmlOutputGenerator.Builder().out(out).uriBase(DEFAULT_URI_BASE).build();
 		try {
 			dp.addInput(new FileInput(file, contentType));
+			app.createDocumentation(dp, generator, Profile.TECHNICAL); // FIXME this should be customize
+		} catch (ReaderException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} catch (TransformerException e) {
+			throw new RuntimeException(e);
+		}
+		
+		return out.toString();
+	}
+
+	public String exec(Map<File, String> mapFileContentType) {
+		System.out.println("Exec Parrot plugin for a list of files"); // DEBUG
+		app = new ParrotAppServ();
+		for (File f : mapFileContentType.keySet()){
+			try {
+				System.out.println("Documenting file:" + f.getCanonicalPath());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				throw new RuntimeException(e);
+				
+			}
+		}
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		DocumentaryProject dp = DocumentaryProjectFactory.createDocumentaryProject(DEFAULT_LOCALE);
+		HtmlOutputGenerator generator = new HtmlOutputGenerator.Builder().out(out).uriBase(DEFAULT_URI_BASE).build();
+		try {
+			for (File file : mapFileContentType.keySet()){
+				dp.addInput(new FileInput(file, mapFileContentType.get(file)));
+			}
 			app.createDocumentation(dp, generator, Profile.TECHNICAL); // FIXME this should be customize
 		} catch (ReaderException e) {
 			throw new RuntimeException(e);

c585d988e6707d39fef7d458659bb25805f6fffe
dayures <dayures@localhost>
2011-08-26 10:54:07 +0200

    Added initial plugin for neontoolkit (and terminae)

---
+++ parrot-eclipse/plugins/neontoolkit/src/es/ctic/parrot/eclipse/neontoolkit/ui/Activator.java
@@ -0,0 +1,68 @@
+package es.ctic.parrot.eclipse.neontoolkit.ui;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+import es.ctic.parrot.eclipse.neontoolkit.utils.ParrotCoreWrapper;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "es.ctic.parrot.eclipse.neontoolkit.ui"; //$NON-NLS-1$
+
+	// The shared instance
+	private static Activator plugin;
+	
+	// Parrot core binding
+	public ParrotCoreWrapper parrotCore;
+	
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+		parrotCore = new ParrotCoreWrapper();
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns an image descriptor for the image file at the given
+	 * plug-in relative path
+	 *
+	 * @param path the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+}
---
+++ parrot-eclipse/plugins/neontoolkit/src/es/ctic/parrot/eclipse/neontoolkit/views/DocBrowserView.java
@@ -0,0 +1,56 @@
+package es.ctic.parrot.eclipse.neontoolkit.views;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.SWTError;
+import org.eclipse.swt.browser.*;
+import org.eclipse.swt.widgets.Composite;
+//import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.part.ViewPart;
+import org.eclipse.core.runtime.Status;
+
+import es.ctic.parrot.eclipse.neontoolkit.ui.Activator;
+
+public class DocBrowserView extends ViewPart {
+	
+	private Browser browser;
+	//private String initialHTML = "<html><head></head><body>Click Parrot -> Generate</body></html>";
+	
+	public static final String ViewID = "es.ctic.parrot.parrot-eclipse.browser";
+
+	public DocBrowserView() {	
+	}
+	
+	public void setBrowserHTML(String html){
+		if (browser != null) {
+			browser.setText(html);
+		}
+	}
+	
+	@Override
+	public void createPartControl(Composite parent) {
+		this.createBrowser(parent);
+		this.setLocationListener();
+	}
+
+	private void createBrowser(Composite parent){
+		try {
+			browser = new Browser(parent, SWT.None);
+		}
+		catch(SWTError e){
+			String msg = "Unable to create browser instance";
+			Status status = new Status(Status.ERROR, Activator.PLUGIN_ID, msg);
+			Activator.getDefault().getLog().log(status);
+			return; 
+		}
+//		this.setBrowserHTML(initialHTML);
+	}
+	
+	private void setLocationListener() {
+	}
+	
+	@Override
+	public void setFocus() {
+		browser.setFocus();
+	}
+
+}

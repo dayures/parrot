24197c3ca5655b1d71ae7a9ba879fdeb257a5458
dayures <dayures@localhost>
2011-10-25 10:14:11 +0200

    added feedback button in the report

--- parrot-web/src/main/webapp/report/js/javascript.js
+++ parrot-web/src/main/webapp/report/js/javascript.js
@@ -132,4 +132,22 @@ jQuery(document).ready(function(){
           style: 'parrot'
       });
       
+  	$('#feedback').feedback({
+		'position': 'left',
+		'mouseoffColor': '#8470ff'
+	});
+	
+    $('#feedback-dialog').dialog({
+        autoOpen: false,
+		modal: true,
+		resizable: false,
+		draggable: false
+	});
+    
+    $('#feedback').click(function() {
+        $('#feedback-dialog').dialog('open');
+        $('#feedback-dialog a').blur(); // remove manually the focus on a elements
+		return false;
+    });
+      
 })

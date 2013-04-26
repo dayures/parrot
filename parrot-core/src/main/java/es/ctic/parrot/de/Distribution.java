fab727622644bdca514c0a21007d5a48084c66c8
Carlos Tejo <dayures@users.noreply.github.com>
2013-04-26 10:01:01 +0200

    support dcat:accessURL and dcat:downloadURL

--- parrot-core/src/main/java/es/ctic/parrot/de/Distribution.java
+++ parrot-core/src/main/java/es/ctic/parrot/de/Distribution.java
@@ -3,5 +3,16 @@ package es.ctic.parrot.de;
 
 public interface Distribution extends DocumentableObject, Versionable{
   
-
+	
+	/**
+	 * Returns the access URL.
+	 * @return the access URL.
+	 */
+	public abstract String getAccessURL();
+	
+	/**
+	 * Returns the download URL.
+	 * @return the download URL.
+	 */
+	public abstract String getDownloadURL();
 }

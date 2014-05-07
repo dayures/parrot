59e75dc77e84d0dd5069712e4c19c80cfdca14d9
Carlos Tejo <carlos.tejo@fundacionctic.org>
2014-05-07 16:11:46 +0200

    show the compilation of tips

--- parrot-web/src/main/webapp/javascript/help-scripts.js
+++ parrot-web/src/main/webapp/javascript/help-scripts.js
@@ -1,3 +1,17 @@
 jQuery(document).ready(function(){
     $('#header').corner();
+	
+    
+	var alltips = new Array();
+	$.get("tipOfTheDay.xml", {}, function(xml){
+		$("tip",xml).each(function(i) {
+			alltips.push($(this).text());
+		});
+
+		for(i in alltips){
+			$('#alltips').append("<p class='tip-compilation'>"+alltips[i]+"</p>")
+		}
+	});
+	
 });
+
